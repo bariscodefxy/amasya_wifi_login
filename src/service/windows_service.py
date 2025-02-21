47a0c9774990886ecdbc5b1b3d83718b23cfa4ed
bariscodefx <root@bariscodefx.com.tr>
2025-02-21 14:42:05 +0300

    add windows_service.py

---
+++ src/service/windows_service.py
@@ -0,0 +1,27 @@
+import win32service
+import win32serviceutil
+import win32event
+import servicemanager
+
+class MyService(win32serviceutil.ServiceFramework):
+    _svc_name_ = "Amasya Wifi Login"
+    _svc_display_name_ = "Amasya Wifi Login's Windows Service"
+    
+    def __init__(self, args):
+        super().__init__(args)
+        self.stop_event = win32event.CreateEvent(None, 0, 0, None)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.stop_event)
+
+    def SvcDoRun(self):
+        servicemanager.LogInfoMsg("My Python Service is starting...")
+        while True:
+            # Your service logic here
+            if win32event.WaitForSingleObject(self.stop_event, 5000) == win32event.WAIT_OBJECT_0:
+                break
+        servicemanager.LogInfoMsg("My Python Service is stopping...")
+
+if __name__ == "__main__":
+    win32serviceutil.HandleCommandLine(MyService)
